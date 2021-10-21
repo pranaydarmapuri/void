583ecf4b9bd54364fab76562bb7f1093dd603bd9
Pranay kumar <61114507+pranaydarmapuri@users.noreply.github.com>
2021-10-21 12:26:19 +0530

    text component done!

--- components/hero-section/index.tsx
+++ components/hero-section/index.tsx
@@ -8,8 +8,20 @@ const HeroSection = () => {
     <div className={css.hero_container}>
       <IntroSection />
       <HeroIllustration />
+      <Text color="secondary" tag="heading5">Hello</Text>
       <Text color="text-secondary" tag="heading5">Hello</Text>
       <Text color="text-primary" tag="heading5">Hello</Text>
+      <Text color="primary" tag="heading5">Hello</Text>
+      <Text color="text-primary" tag="heading4">Hello</Text>
+      <Text color="text-secondary" tag="heading4">Hello</Text>
+      <Text color="primary" tag="heading4">Hello</Text>
+      <Text color="secondary" tag="heading4">Hello</Text>
+      <Text tag="title">Terms & Conditions</Text>
+      <Text color="text-secondary" tag="title">Terms & Conditions</Text>
+      <Text color="muted" tag="title">Terms & Conditions</Text>
+      <Text color="primary" tag="title">Terms & Conditions</Text>
+      <Text color="secondary" tag="title">Terms & Conditions</Text>
+      <Text tag="title">What we <Text color="primary">do</Text>.</Text>
     </div>
   )
 }
